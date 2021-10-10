7499b8c0fa998aec0b9e712e0373c5f721f77021
Zeke Sikelianos <zeke@sikelianos.com>
2021-10-09 21:15:04 -0700

    add PyPI classifiers

--- setup.py
+++ setup.py
@@ -6,4 +6,14 @@ setup(
   description='Annotate your requirements.txt file with a short summary of each package.',
   py_modules=["summarize_requirements"],
   package_dir={'': 'src'},
+  classifiers=[
+    "Environment :: Console",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Programming Language :: Python",
+    "Topic :: Documentation",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ]
 )
